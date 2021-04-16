1f55faed7aa1fcb24f81aba2266a5f525a68faec
Emma F <esfoures@gmail.com>
2021-04-16 18:46:49 -0400

    Phone screen layout added

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
-console.log("Hello"); 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express"); 
+const exphbs = require('express-handlebars'); 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const lessons = require("./models/lessons"); 
+const { getAllLessons } = require("./models/lessons"); 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const app = express(); 
+
+app.engine('handlebars', exphbs()); 
+app.set('view engine', 'handlebars'); 
+
+app.use(express.static("public")); 
+
+// HOME 
+app.get("/", (req, res)=> {
+    res.render("index", {
+        title: "Home", 
+        // data: lessons.getAllLessons()
+    })
+}); 
+
+// PRACTICE
+app.get("/practice", (req, res)=> {
+    res.render("practice", {
+        title: "Practice", 
+        // data: lessons.getAllLessons()
+    })
+}); 
+
+// LESSONS
+app.get("/lessons", (req, res)=> {
+    res.render("lessons", {
+        title: "Lessons", 
+        // data: lessons.getAllLessons()
+    })
+}); 
+
+const PORT = 3000; 
+
+app.listen(PORT,()=>{
+    console.log(`Web Server is up and running, port ${PORT}`);    
+});
